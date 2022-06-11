65f909fdc4f4de0b523de9fbc65bac0c9b26fe75
Santiago <santiago.zibe@gmail.com>
2022-06-11 16:03:05 -0300

    useAnimation

---
+++ src/hooks/useAnimation.tsx
@@ -0,0 +1,59 @@
+import { useRef } from "react";
+import { Animated, Easing } from "react-native";
+
+const useAnimation = () => {
+
+     // Es preferible manejar las animaciones con useRef para que no re-renderice los componentes
+     // el .Value es para manejar un solo valor
+     // el .ValueXY es para manejar 2 valores
+     const opacity = useRef(new Animated.Value(0)).current;
+     const position = useRef(new Animated.Value(0)).current;
+
+     const fadeIn = () => {
+          Animated.timing(
+               opacity,
+               {
+                    toValue: 1,
+                    duration: 300,
+                    useNativeDriver: true,
+               },
+          ).start(() => console.log('La animacion termino'));
+     };
+
+     const fadeOut = () => {
+          Animated.timing(
+               opacity,
+               {
+                    toValue: 0,
+                    duration: 300,
+                    useNativeDriver: true,
+               },
+          ).start();
+     };
+
+     const startMovingPosition = (initPosition: number, duration: number = 300) => {
+
+          position.setValue(initPosition);
+
+          Animated.timing(
+               position,
+               {
+                    toValue: 0,
+                    duration,
+                    useNativeDriver: true,
+                    easing: Easing.bounce,
+               }
+          ).start(() => console.log('La animacion termino'));
+     };
+
+     return {
+          fadeIn,
+          fadeOut,
+          opacity,
+          position,
+          startMovingPosition,
+     };
+
+};
+
+export default useAnimation;
--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,11 +1,44 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Animated, Button } from 'react-native';
 import React from 'react';
+import useAnimation from '../hooks/useAnimation';
 
 const Animation101Screen = () => {
+
+     const { opacity, position, fadeIn, fadeOut, startMovingPosition } = useAnimation();
+
      return (
           <View style={styles.container}>
 
-               <View style={styles.purpleBox} />
+               {/* Para invocar las animaciones se utiliza Animated.View para que sean compatibles*/}
+               <Animated.View style={{
+                    ...styles.purpleBox,
+                    marginBottom: 20,
+                    opacity,
+                    transform: [{
+                         translateY: position,
+                    }],
+               }} />
+
+               <View style={{ flexDirection: 'row', backgroundColor: '#5856D6' }}>
+
+                    <View style={{ margin: 5 }}>
+                         <Button
+                              title="FadeIn"
+                              onPress={() => {
+                                   fadeIn();
+                                   startMovingPosition(-200, 1000);
+                              }}
+                         />
+                    </View>
+
+                    <View style={{ margin: 5 }}>
+                         <Button
+                              title="FadeOut"
+                              onPress={fadeOut}
+                         />
+                    </View>
+
+               </View>
 
           </View>
      );
