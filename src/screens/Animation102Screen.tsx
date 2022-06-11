572cdf0bbde68b3164f76111218e3c54bae66438
Santiago <santiago.zibe@gmail.com>
2022-06-11 17:02:00 -0300

    Animated.ValueXY

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,10 +1,38 @@
-import { View, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, StyleSheet, Animated, PanResponder } from 'react-native';
+import React, { useRef } from 'react';
 
 const Animation102Screen = () => {
+
+     const moveAway = useRef(new Animated.ValueXY()).current;
+
+     const moveAwayResponder = PanResponder.create({
+          onStartShouldSetPanResponder: () => true,
+          onPanResponderMove: Animated.event([
+               null,
+               {
+                    dx: moveAway.x,
+                    dy: moveAway.y,
+               },
+          ],
+               { useNativeDriver: false }
+          ),
+          // Cuando el usuario suelta el objeto
+          onPanResponderRelease: () => {
+               Animated.spring(
+                    moveAway,
+                    {
+                         toValue: { x: 0, y: 0 },
+                         useNativeDriver: false,
+                    }
+               ).start();
+          },
+     });
+
      return (
-          <View style={{ flex: 1 }}>
-               <View style={styles.purpleBox} />
+          <View style={styles.container}>
+               <Animated.View
+                    {...moveAwayResponder.panHandlers}
+                    style={[moveAway.getLayout(), styles.boxContent]} />
           </View>
      );
 };
@@ -12,8 +40,14 @@ const Animation102Screen = () => {
 export default Animation102Screen;
 
 const styles = StyleSheet.create({
-     purpleBox: {
+     container: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+     },
+     boxContent: {
           backgroundColor: '#5856',
+          borderRadius: 100,
           width: 150,
           height: 150,
      },
