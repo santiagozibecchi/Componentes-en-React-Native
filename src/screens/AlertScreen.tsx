160461ead4c41ffa1a901fc3455a90d3ee6040ce
Santiago <santiago.zibe@gmail.com>
2022-06-12 08:45:50 -0300

    Prompt

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,8 @@
-import { View, Text, Button, Alert } from 'react-native';
 import React from 'react';
+import { View, Button, Alert } from 'react-native';
+
+import prompt from 'react-native-prompt-android';
+
 import HeaderTitle from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
@@ -28,6 +31,33 @@ const AlertScreen = () => {
           );
      };
 
+     const showPrompt = () => {
+          // funciona solo en IOS
+          // Alert.prompt(
+          //      'Esta seguro?',
+          //      'Esta accion no se puede revertir',
+          //      (valor: string) => console.log('info:', valor), // valor en realidad es de tipo object
+          //      'plain-text',
+          //      'Hola mundo', // placeholder
+          //      'number-pad' // email-adress
+          // );
+
+          prompt(
+               'Enter password',
+               'Enter your password to claim your $1.5B in lottery winnings',
+               [
+                    { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                    { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+               ],
+               {
+                    type: 'secure-text',
+                    cancelable: false,
+                    defaultValue: 'test',
+                    placeholder: 'placeholder',
+               }
+          );
+     };
+
 
      return (
           <View style={styles.globalMargin}>
@@ -36,6 +66,13 @@ const AlertScreen = () => {
                <Button
                     title="Mostrar alerta"
                     onPress={showAlert}
+               />
+
+               <View style={{ height: 10 }} />
+
+               <Button
+                    title="Mostrar alerta"
+                    onPress={showPrompt}
                />
 
           </View>
