b324ff7aee4d7e08bd7c97b5b9be7b9d08e2604e
Santiago <santiago.zibe@gmail.com>
2022-06-17 19:15:41 -0300

    inifity scroll img

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -1,14 +1,73 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, StyleSheet, Image, FlatList, ActivityIndicatorComponent, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
 import HeaderTitle from '../components/HeaderTitle';
-import { styles } from '../theme/appTheme';
 
 const InfiniteScrollScreen = () => {
+
+     const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+     const loadMore = () => {
+
+          const newArray: number[] = [];
+
+          for (let i = 0; i < 5; i++) {
+               newArray[i] = numbers.length + i;
+          }
+          setNumbers([...numbers, ...newArray]);
+
+     };
+
+     const renderItem = (item: number) => {
+          return (
+               <Image
+                    source={{ uri: `https://picsum.photos/id/${item}/400/300` }}
+                    style={{
+                         width: '100%',
+                         height: 300,
+                    }}
+               />
+          );
+     };
+
      return (
-          <View style={styles.globalMargin}>
-               <HeaderTitle title="Infite Scroll" />
+          <View style={{ flex: 1, backgroundColor: 'red' }}>
+
+               <FlatList
+                    data={numbers}
+                    keyExtractor={(item) => item.toString()}
+                    renderItem={({ item }) => renderItem(item)}
+
+                    ListHeaderComponent={<HeaderTitle title="Infite Scroll" />}
+
+                    // cuando estoy en el final
+                    onEndReached={loadMore}
+                    // A qué distancia del final, debe estar el borde inferior de la lista desde el final
+                    // del contenido para activar la devolución de llamada onEndReached
+                    onEndReachedThreshold={0.5}
+
+                    ListFooterComponent={() => (
+                         <View style={{
+                              height: 60,
+                              width: '100%',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                         }}>
+                              <ActivityIndicator size={40} color="5856D6" />
+                         </View>
+                    )}
+
+
+               />
+
           </View>
      );
 };
+
+const styles = StyleSheet.create({
+     textItem: {
+          backgroundColor: 'green',
+          height: 150,
+     },
+});
 
 export default InfiniteScrollScreen;
